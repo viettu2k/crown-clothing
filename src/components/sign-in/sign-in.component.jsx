5b52dcb62be8d151b4e30c8ead758739f428103c
unknown <doanviettu2k@gmail.com>
2021-05-13 09:17:21 +0700

    updating our app to redirect to home if user is signed in

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -33,16 +33,15 @@ class SignIn extends React.Component {
   handleChange = (event) => {
     const { value, name } = event.target;
 
-    this.setState({
-      [name]: value,
-    });
+    this.setState({ [name]: value });
   };
 
   render() {
     return (
       <div className="sign-in">
-        <h2> I already have an account </h2>
-        <span> Sign in with your email and password </span>
+        <h2>I already have an account</h2>
+        <span>Sign in with your email and password</span>
+
         <form onSubmit={this.handleSubmit}>
           <FormInput
             name="email"
