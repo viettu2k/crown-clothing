18b6dcbeb37c0f10373dc9c7149fd1b300447937
unknown <doanviettu2k@gmail.com>
2021-05-13 10:41:16 +0700

    added cartIcon and cartDropdown components to our application, also implement cart reducer

--- src/components/collection-item/collection-item.component.jsx
+++ src/components/collection-item/collection-item.component.jsx
@@ -9,11 +9,11 @@ const CollectionItem = ({ id, name, price, imageUrl }) => (
       style={{
         backgroundImage: `url(${imageUrl})`,
       }}
-    />
+    />{" "}
     <div className="collection-footer">
-      <span className="name">{name}</span>
-      <span className="price">{price}$</span>
-    </div>
+      <span className="name"> {name} </span>{" "}
+      <span className="price"> {price} </span>{" "}
+    </div>{" "}
   </div>
 );
 
