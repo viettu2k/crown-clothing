977a3cf92d8780f3a5712a214e8a136dd9af5377
unknown <doanviettu2k@gmail.com>
2021-05-16 15:42:18 +0700

    removing redux-logger from production build

--- src/components/collections-overview/collections-overview.component.jsx
+++ src/components/collections-overview/collections-overview.component.jsx
@@ -4,20 +4,21 @@ import { createStructuredSelector } from "reselect";
 
 import CollectionPreview from "../collection-preview/collection-preview.component";
 
-import { selectCollections } from "../../redux/shop/shop.selectors";
+import { selectCollectionsForPreviews } from "../../redux/shop/shop.selectors";
 
 import "./collections-overview.styles.scss";
 
 const CollectionsOverview = ({ collections }) => (
   <div className="collections-overview">
+    {" "}
     {collections.map(({ id, ...otherCollectionProps }) => (
       <CollectionPreview key={id} {...otherCollectionProps} />
-    ))}
+    ))}{" "}
   </div>
 );
 
 const mapStateToProps = createStructuredSelector({
-  collections: selectCollections,
+  collections: selectCollectionsForPreviews,
 });
 
 export default connect(mapStateToProps)(CollectionsOverview);
