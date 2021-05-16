6746035fe610c8bcb54cd024ec6e60a47f28644b
unknown <doanviettu2k@gmail.com>
2021-05-16 10:39:50 +0700

    updating stripe intergration and updating our favicon and title

---
+++ src/components/stripe-button/stripe.component.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import StripeCheckout from "react-stripe-checkout";
+
+const StripeCheckoutButton = ({ price }) => {
+  const priceForStripe = price * 100;
+  const publishableKey =
+    "pk_test_51Ira4aJlFs0TYKlg0LvnitewXm8rpy8sqzfkPLmB9lPjakNzbg4zlReXUCVQKIdBpHrGs9tr43gGyohFWk9Qs0h700ADQTdDHs";
+
+  const onToken = (token) => {
+    console.log(token);
+    alert("Payment Successful");
+  };
+
+  return (
+    <StripeCheckout
+      label="Pay Now"
+      name="CROWN Clothing Ltd."
+      billingAddress
+      shippingAddress
+      description={`Your total is $${price}`}
+      amount={priceForStripe}
+      panelLabel="Pay Now"
+      token={onToken}
+      stripeKey={publishableKey}
+    />
+  );
+};
+
+export default StripeCheckoutButton;
