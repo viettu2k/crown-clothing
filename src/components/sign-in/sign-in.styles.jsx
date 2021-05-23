1f35e18aa272b91358805a019724dc0147a72cb2
viettu2k <doanviettu2k@gmail.com>
2021-05-23 09:46:15 +0700

    created user and cart sagas for sign in, sign up and sign in with google

---
+++ src/components/sign-in/sign-in.styles.jsx
@@ -0,0 +1,17 @@
+import styled from "styled-components";
+
+export const SignInContainer = styled.div `
+  font-family: "Operator Mono Lig";
+  width: 380px;
+  display: flex;
+  flex-direction: column;
+`;
+
+export const SignInTitle = styled.h2 `
+  margin: 10px 0;
+`;
+
+export const ButtonsBarContainer = styled.div `
+  display: flex;
+  justify-content: space-between;
+`;
---
+++ src/components/sign-up/sign-up.styles.jsx
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+export const SignUpContainer = styled.div `
+  font-family: "Operator Mono Lig";
+  display: flex;
+  flex-direction: column;
+  width: 380px;
+`;
+
+export const SignUpTitle = styled.h2 `
+  margin: 20px 0;
+`;
